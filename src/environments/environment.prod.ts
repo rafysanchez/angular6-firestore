5eda6f99591106bbdd4915e4b50b05d94cffd47b
rafysanchez <rafysanchez@hotmail.com>
2018-09-19 15:50:38 -0300

    adjs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDVnNKJXl8oDTaXeWDkFp_fGP_gu7A6P70',
-    authDomain: 'angular6-aff3f.firebaseapp.com',
-    databaseURL: 'https://angular6-aff3f.firebaseio.com',
-    projectId: 'angular6-aff3f',
-    storageBucket: 'angular6-aff3f.appspot.com',
-    messagingSenderId: '266534621501'
+    apiKey: 'AIzaSyBDtm6x8qcjsJEHnDeVdP7KX6xJWmyCtaE',
+    authDomain: 'products-f31d4.firebaseapp.com',
+    databaseURL: 'https://products-f31d4.firebaseio.com',
+    projectId: 'products-f31d4',
+    storageBucket: 'products-f31d4.appspot.com',
+    messagingSenderId: '146376711248'
   }
 };
